73ad2b760bbe17f2e717b002bad7ce1e0bf5f627
newwares <45801973+waresnew@users.noreply.github.com>
2025-12-30 16:21:10 -0500

    transaparency arg

--- src/cli.rs
+++ src/cli.rs
@@ -5,6 +5,7 @@ pub struct Args {
     pub input: String,
     pub output: String,
     pub stream: bool,
+    pub transparency_threshold: u32,
 }
 #[allow(clippy::derivable_impls)]
 impl Default for Args {
@@ -13,63 +14,82 @@ impl Default for Args {
             input: Default::default(),
             output: Default::default(),
             stream: false,
+            transparency_threshold: 5,
         }
     }
 }
 
-fn print_help(program_name: &str) {
-    let help_message = format!(
+fn get_help_msg(program_name: &str) -> String {
+    format!(
         r#"
 https://github.com/waresnew/gif-compressor
 
 Usage:
 {} [arguments]
 
-Mandatory arguments:
-  -i FILE       Specify the input file.
-  -o FILE       Specify the output file.
-
-Optional arguments:
-  --stream      Instructs the program to not store all GIF frames in memory at once. Leads to reduced peak memory usage at the cost of longer runtime.
+  -i, --input FILE               (Mandatory) Specify the input file path.
+  -o, --output FILE              (Mandatory) Specify the output file path.
+  -h, --help                     Prints this help message.
+  -s, --stream                   Instructs the program to not store all GIF frames in memory at once. Leads to reduced peak memory usage at the cost of longer runtime.
+  -t, --transparency INTEGER     Specify a non-negative colour distance threshold for transparency optimization. Default: 5
 "#,
         program_name
-    );
-    println!("{help_message}")
+    )
 }
 pub fn parse_args(mut args_raw: env::Args) -> Args {
     let program_name = args_raw.next().unwrap();
+    let help_msg = get_help_msg(&program_name);
     let mut args = Args::default();
     if args_raw.len() == 0 {
-        print_help(&program_name);
-        std::process::exit(0);
+        eprintln!("{help_msg}");
+        std::process::exit(1);
     }
     while let Some(arg) = args_raw.next() {
         match arg.as_str() {
-            "-i" => {
-                args.input = args_raw.next().expect("missing input file");
+            "-i" | "--input" => {
+                args.input = args_raw.next().unwrap_or_else(|| {
+                    eprintln!("missing input path");
+                    std::process::exit(1);
+                })
             }
             "-h" | "--help" => {
-                print_help(&program_name);
+                println!("{help_msg}");
                 std::process::exit(0);
             }
-            "-o" => {
-                args.output = args_raw.next().expect("missing output file");
+            "-o" | "--output" => {
+                args.output = args_raw.next().unwrap_or_else(|| {
+                    eprintln!("missing output path");
+                    std::process::exit(1);
+                });
             }
-            "--stream" => {
+            "--stream" | "-s" => {
                 args.stream = true;
+            }
+            "--transparency" | "-t" => {
+                args.transparency_threshold = args_raw
+                    .next()
+                    .unwrap_or_else(|| {
+                        eprintln!("missing transparency threshold");
+                        std::process::exit(1);
+                    })
+                    .parse()
+                    .unwrap_or_else(|e| {
+                        eprintln!("failed to parse transparency threshold value: {e}");
+                        std::process::exit(1);
+                    })
             }
             "--" => {
                 break;
             }
             x => {
-                println!("unexpected token: {}.\ntry {program_name} -h for help.", x);
-                std::process::exit(0);
+                eprintln!("unexpected token: {}.\ntry {program_name} -h for help.", x);
+                std::process::exit(1);
             }
         }
     }
     if args.input.is_empty() || args.output.is_empty() {
-        println!("input or output file argument is missing");
-        std::process::exit(0);
+        eprintln!("input or output path argument is missing. run {program_name} -h for help.");
+        std::process::exit(1);
     }
     args
 }
